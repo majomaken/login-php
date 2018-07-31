f86cc12ca2da3ecdcd274a19625470be1fbd7383
majomaken <majo.maken@gmail.com>
2018-07-31 09:25:41 -0500

    Modifying the css by adding roboto font and add style form

--- index.php
+++ index.php
@@ -3,8 +3,18 @@
   <head>
     <meta charset="utf-8">
     <title>Welcome to (APP NAME)</title>
+    <!-- Esta es un opcion para traer diferentes fuentes. Lo mejor que podemos hacer
+    seria crear una carpeta /font para guardar un archivo con los links y llamarlo
+    con php. Esa es una manera mas organizada de hacerlo. -->
+     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
       <h1>Please Login or SignUp</h1>
+      <!-- Con estas dos opciones podremos dejar que el usuario decida que hacer
+      si quiere loguear en nuestra pagina. -->
+      <a href="login.php">Login</a>
+      <!-- O quiere registrarse. -->
+      <a href="signup.php">Sing Up</a>
   </body>
 </html>
---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <h1>Login</h1>
+      <!-- El atributo action es el lugar o la dirección a la cual se va emviar
+      el formulario, por eso es tan importante. -->
+      <!-- Como sabemos hay dos metodos para enviar nustro formulario
+      el post y el get. Hay que tener cuidado con el get ya que la informació
+      del formulario que se envia queda en la URL de nuestra pagin.
+      Con post no pasa esto ya que los archivos se envian al servidor de una manera
+      que no es publica. -->
+      <form class="" action="login.php" method="post">
+            <input type="text" name="email" placeholder="Enter your email">
+            <input type="password" name="password" placeholder="Enter your password">
+            <!-- Este boton de submit es importante y hay que tener en cuenta que el
+            envia el evento a nuestro form. -->
+            <input type="submit" value="Send">
+      </form>
+
+  </body>
+</html>
