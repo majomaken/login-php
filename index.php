b2eb1b30e3936a715407f2a425b7c4fa614fb704
majomaken <majo.maken@gmail.com>
2018-07-31 09:06:00 -0500

    Create new folders:assets, partials. Create new files: database.php, login.php, logout.php, singup.php and basic structure html 5

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-    echo "Works";
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to (APP NAME)</title>
+  </head>
+  <body>
+      <h1>Please Login or SignUp</h1>
+  </body>
+</html>
